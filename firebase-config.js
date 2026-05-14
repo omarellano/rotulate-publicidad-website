8e5dceffcffb5cc32203e1a3843afe30833ca750
Omar Arellano <cotizaciones@rotulatepublicidad.com>
2026-05-14 13:51:03 -0500

    Configuración real de Firebase añadida

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 /**
  * Firebase Configuration for Rotulate Publicidad
- * Reemplaza los valores con los de tu consola de Firebase
  */
 
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROYECTO_ID",
-    storageBucket: "TU_PROYECTO.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyD_dgRYZMJEmFKp9RcIdRqPEJrb8dZnkE8",
+  authDomain: "rotulate-publicidad.firebaseapp.com",
+  projectId: "rotulate-publicidad",
+  storageBucket: "rotulate-publicidad.firebasestorage.app",
+  messagingSenderId: "700007448429",
+  appId: "1:700007448429:web:be0e07ef403fedebb56464",
+  measurementId: "G-7RD98QCP79"
 };
 
 // Inicializar Firebase (Placeholder, se usará en index.html)
